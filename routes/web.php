3161eee08b1e7afd93a758da2b54598c2a1e05a7
monsalman <salmanfirdaus.m@gmail.com>
2024-10-01 16:22:24 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\DashboardController; // Import DashboardController
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     if (Auth::check()) {
@@ -21,11 +21,13 @@ Route::get('login', [AuthController::class, 'index'])->name('login');
 Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
 Route::get('registration', [AuthController::class, 'registration'])->name('register');
 Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard'); 
-Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('dashboardadmin', [DashboardController::class, 'adminIndex'])->name('dashboardadmin');
 
-// Tambahkan rute untuk mengedit dan menghapus pengguna
-Route::get('users/{id}/edit', [AuthController::class, 'edit'])->name('users.edit');
-Route::put('users/{id}', [AuthController::class, 'update'])->name('users.update');
-Route::delete('users/{id}', [AuthController::class, 'destroy'])->name('users.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard'); 
+    Route::get('dashboardadmin', [DashboardController::class, 'adminIndex'])->name('dashboardadmin');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('users/{id}/edit', [AuthController::class, 'edit'])->name('users.edit');
+    Route::put('users/{id}', [AuthController::class, 'update'])->name('users.update');
+    Route::delete('users/{id}', [AuthController::class, 'destroy'])->name('users.destroy');
+});
