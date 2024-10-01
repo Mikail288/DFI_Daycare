2cd6edebba6e7e1a03680d8e6180cf7b5b0952aa
monsalman <salmanfirdaus.m@gmail.com>
2024-10-01 16:40:15 +0700

    tambah limit untuk user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User; // Import model User atau model lain yang ingin ditampilkan
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -14,7 +15,13 @@ class DashboardController extends Controller
 
     public function adminIndex()
     {
-        $users = User::all(); // Ambil semua data dari tabel users
-        return view('dashboardadmin', compact('users'));
+        // Cek apakah pengguna yang sedang login adalah admin
+        if (Auth::check() && Auth::user()->role == 'admin') {
+            $users = User::all(); // Ambil semua data dari tabel users
+            return view('dashboardadmin', compact('users'));
+        }
+
+        // Jika bukan admin, redirect ke halaman login dengan pesan error
+        return redirect("login")->withErrors('Oops! You do not have access to the admin dashboard');
     }
 }
